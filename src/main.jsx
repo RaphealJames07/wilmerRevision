5fd9561ccd162211e4a53023bd0cab0e8acb223e
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2025-03-24 18:24:32 +0100

    two by chris brown

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import {Provider} from "react-redux";
-import {PersistGate} from "redux-persist/integration/react";
+import {PersistGate} from "redux-persist/es/integration/react";
 import {persistStore} from "redux-persist";
 import store from "./global/store.js";
 import "owl.carousel/dist/assets/owl.carousel.css";
